848dd8cbe17c300efeecba1931364627fc6b2256
iamdamified <119352851+iamdamified@users.noreply.github.com>
2023-05-09 19:00:28 +0100

    add APIs

--- API_App/urls.py
+++ API_App/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import api_home_page
+from .views import api_home_page, api_detail_page, api_update_page, api_delete_page, api_create_page
 
 urlpatterns = [
-    path("", api_home_page, name="api-home-page")
+    path("", api_home_page, name="api-home-page"),
+    path("create/", api_create_page, name="create-page"),
+    path("<int:id>/detail/", api_detail_page, name="detail-page"),
+    path("<int:id>/update/", api_update_page, name="update-page"),
+    path("<int:id>/delete/", api_delete_page, name="delete-page")
 ]
--- API_App/views.py
+++ API_App/views.py
@@ -11,3 +11,36 @@ def api_home_page(request):
     new_serialized_data = PostSerializers(all_posts, many=True)# Serializers converts one object at a time, in this case we have a more than one, hence many=True must be added
     return Response(new_serialized_data.data)
 
+
+@api_view(["GET"])
+def api_detail_page(request, id):
+    single_post = Post.objects.get(id=id) # A query set
+    serialized_post = PostSerializers(single_post)# Serializers converts one object at a time, in this case we have a more than one, hence many=True must be added
+    return Response(serialized_post.data)
+
+
+@api_view(["PUT"])
+def api_update_page(request, id):
+    single_post = Post.objects.get(id=id) # A query set
+    new_data = request.data #collect new data and hold
+    serialized_new_data = PostSerializers(single_post, data=new_data, partial=True)#
+    if serialized_new_data.is_valid():
+        serialized_new_data.save()
+        return Response(serialized_new_data.data)
+    else:
+        return Response({"Error": "You typed rubbish!!"})
+    
+
+@api_view(["DELETE"])
+def api_delete_page(request, id):
+    single_post = Post.objects.get(id=id) # A query set
+    single_post.delete() #delete
+    return Response({"Success": "Post has been successfully deleted"})
+
+
+@api_view(["POST"])
+def api_create_page(request):
+    new_data = PostSerializers(data=request.data)
+    if new_data.is_valid():
+        new_data.save()
+        return Response(new_data.data)
